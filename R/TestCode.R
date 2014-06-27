85b3829dfaffe834183916f4354565ba9d11aa97
schuemie <schuemie@ohdsi.org>
2014-06-27 08:18:06 -0400

    Changed SQL->CCD interface so stratum_id is taken from row_id for unstratified analyses. Also added preference score plot.

--- R/TestCode.R
+++ R/TestCode.R
@@ -29,14 +29,49 @@ testCode <- function(){
   
   covariateSql <-"SELECT * FROM #ccd_covariate_input_for_ps ORDER BY stratum_id, row_id, covariate_id"
   covariateSql <- translateSql(covariateSql,"sql server",connectionDetails$dbms)$sql
-  
+ 
   writeLines("Loading data for propensity model")
   startQuery <- Sys.time()
-  ccdData <- dbGetCcdInput(conn,outcomeSql,covariateSql,modelType = "clr")
-  delta <- Sys.time() - startQuery
+  ccdData <- dbGetCcdInput(conn,outcomeSql,covariateSql,modelType = "lr",addIntercept=TRUE)
+  ccdelta <- Sys.time() - startQuery
   writeLines(paste("Load took", signif(delta,3), attr(delta,"units")))
   
   writeLines("Fitting propensity model")
-  ccdFit <- fitCcdModel(ccdData, prior = prior("normal",0.01))
+  startQuery <- Sys.time()
+  ccdFit <- fitCcdModel(ccdData, prior = prior("laplace",0.1))
+  delta <- Sys.time() - startQuery
+  writeLines(paste("Fitting took", signif(delta,3), attr(delta,"units")))
+  #sum(coef(ccdFit)>0.1) 
+
+  p <- predict(ccdFit)
   
+  #Show preference score distributions:
+  ySql <-"SELECT row_id,y FROM #ccd_outcome_input_for_ps"
+  ySql <- translateSql(ySql,"sql server",connectionDetails$dbms)$sql
+  y <- dbGetQuery(conn,ySql)
+  colnames(y) <- tolower(colnames(y))
+  p <- data.frame(pred = p, row_id = as.numeric(attr(p,"names")))
+  #compute preference score:
+  prop <- sum(y$y / nrow(y))
+  interm <- exp(log(p$pred/(1-p$pred)) - log(prop/(1-prop)))
+  p$pref <- interm / (interm+1)
+  m <- merge(y,p,by=c("row_id"))
+  
+  library(ggplot2)
+  ggplot(m, aes(x=pref,color=as.factor(y),group=as.factor(y),fill=as.factor(y))) + 
+    geom_density() +
+    scale_fill_manual(values=c(rgb(0.8,0,0,alpha=0.5),rgb(0,0,0.8,alpha=0.5))) +
+    scale_color_manual(values=c(rgb(0.8,0,0,alpha=0.5),rgb(0,0,0.8,alpha=0.5)))
+  
+  
+  dbDisconnect(conn)
+  
+  #to store temp tables:
+  #dbSendUpdate(conn,"CREATE TABLE temp_outcomes AS SELECT * FROM ccd_outcome_input_for_ps")
+  #dbSendUpdate(conn,"CREATE TABLE temp_covariates AS SELECT * FROM ccd_covariate_input_for_ps")
+  
+  #to restore temp tables:
+  #dbSendUpdate(conn,"SET SEARCH_PATH TO cdm4_sim")
+  #dbSendUpdate(conn,"CREATE TEMP TABLE ccd_outcome_input_for_ps AS SELECT * FROM temp_outcomes")
+  #dbSendUpdate(conn,"CREATE TEMP TABLE ccd_covariate_input_for_ps AS SELECT * FROM temp_covariates")
 }
