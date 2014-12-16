1dab47211e4c133df351c0c837741382385e47aa
schuemie <schuemie@ohdsi.org>
2014-12-16 03:17:46 -0500

    Tidying up example code, switched to snowfall.

--- R/TestRunOneAnalysis.R
+++ R/TestRunOneAnalysis.R
@@ -1,15 +1,15 @@
 testRunOneAnalysis <- function(){
   library(CohortMethod)
-  library(snow)
+  library(snowfall)
   setwd("c:/temp")
   options("fftempdir" = "c:/temp")
-  cluster <- makeCluster(16)
+  sfInit(parallel=TRUE, cpus=32)
   
   pw <- NULL
   dbms <- "sql server"
   user <- NULL
-  server <- "RNDUSRDHIT07"
-  cdmSchema <- "CDM_Truven_MDCR"
+  server <- "RNDUSRDHIT05"
+  cdmSchema <- "CDM_optum"
   resultsSchema <- "scratch"
   port <- NULL
   
@@ -28,12 +28,12 @@ from concept_ancestor
 where ancestor_concept_id in (40241331, 21600960)
 "
   excludeSql <- SqlRender::translateSql(excludeSql,targetDialect = connectionDetails$dbms)$sql
-  excludedCovariates <- dbGetQuery(conn,excludeSql)
-  save(excludedCovariates,file="mdcrExcludedCovariates")
+  excludedCovariates <- querySql(conn,excludeSql)
+  save(excludedCovariates,file="optumExcludedCovariates")
   
-  sql <- "SELECT cohort_definition_id,count(subject_id) as num_persons FROM [Scratch].[dbo].[MDCR_RivaWarfCER_cohort] group by cohort_definition_id order by cohort_definition_id"
-  x <-querySql(connect(connectionDetails),sql)
-  ###
+  sql <- "SELECT cohort_definition_id,count(subject_id) as num_persons FROM [Scratch].[dbo].[Optum_RivaWarfCER_cohort] group by cohort_definition_id order by cohort_definition_id"
+  cohortIds <- querySql(conn,sql)
+  cohortIds <- cohortIds$COHORT_DEFINITION_ID[cohortIds$COHORT_DEFINITION_ID > 2]
   
   cohortData <- getDbCohortData(connectionDetails,cdmSchema=cdmSchema,resultsSchema=resultsSchema,
                                 targetDrugConceptId = 1,
@@ -44,13 +44,13 @@ where ancestor_concept_id in (40241331, 21600960)
                                 studyStartDate = "", 
                                 studyEndDate = "", 
                                 exclusionConceptIds = "",
-                                outcomeConceptIds = c(1000028060,1000134438,1035406331,1035406349,1035406361,1035406391,1035406402,1035506612,1035506621,1035606949,1035607026,1035607337,1035607461,1036110386,1036110587,1036110933,1036110951,1036211101,1036313741,1036314156,1036315380,1036315910,1036315934,1036416637,1036416695,1036516895,1036516905,1036516909,1036617158,1036617163,1036617187,1036617553,1036718555,1036918850,1036918858,1036919212,1037019460,1037119529,1037119539,1037119607,1037320079,1037320098,1037320318,1037420426,1037420593,1037521024,1037522022), 
+                                outcomeConceptIds = cohortIds,
                                 outcomeConditionTypeConceptIds = "", 
                                 maxOutcomeCount = 1,
                                 exposureSchema = resultsSchema,
-                                exposureTable = "MDCR_RivaWarfCER_cohort",
+                                exposureTable = "Optum_RivaWarfCER_cohort",
                                 outcomeSchema = resultsSchema,
-                                outcomeTable = "MDCR_RivaWarfCER_cohort",
+                                outcomeTable = "Optum_RivaWarfCER_cohort",
                                 useCovariateDemographics = TRUE, 
                                 useCovariateConditionOccurrence = TRUE,
                                 useCovariateConditionEra = TRUE, 
@@ -63,25 +63,30 @@ where ancestor_concept_id in (40241331, 21600960)
                                 useCovariateObservation = TRUE,
                                 useCovariateConceptCounts = TRUE, 
                                 useCovariateRiskScores = TRUE,
-                                useCovariateInteractionYear = TRUE, 
-                                useCovariateInteractionMonth = TRUE,
-                                excludedCovariateConceptIds = excludedCovariates$concept_id, 
+                                useCovariateInteractionYear = FALSE, 
+                                useCovariateInteractionMonth = FALSE,
+                                excludedCovariateConceptIds = excludedCovariates$CONCEPT_ID, 
                                 deleteCovariatesSmallCount = 100)
   
-  #saveCohortData(cohortData,"OPTUMCohortDataNegControls4")
+  saveCohortData(cohortData,"OPTUMCohortDataNegControlsNoInteract")
   
-  cohortData <- loadCohortData("OPTUMCohortDataNegControls4") 
+  cohortData <- loadCohortData("OPTUMCohortDataNegControlsNoInteract") 
   outcomeCounts <- summary(cohortData)$outcomeCounts
   outcomeCounts <- outcomeCounts[outcomeCounts$eventCount != 0,]
   
   ps <- createPs(cohortData, 
                  checkSorting = FALSE,
                  outcomeConceptId = outcomeCounts$outcomeConceptId[1],
-                 control = createControl(noise="quiet"))
+                 control = createControl(noise="quiet",lowerLimit = 0.001, upperLimit = 1))
   psVariance = attr(ps,"priorVariance")
-  psVariance <- 0.007
-  clusterExport(cluster,c("cohortData","psVariance"))
+  psVariance <- 0.01
+
+  sfExport("cohortData")
+  sfExport("psVariance")
   runOutcome <- function(outcomeConceptId){
+    if (file.exists(paste("analysis_1/outcomeModel_",outcomeConceptId,".rData",sep="")))
+      return()
+    
     library(CohortMethod)
     ps <- createPs(cohortData, outcomeConceptId = outcomeConceptId, prior=createPrior("laplace",psVariance,exclude = 0))
     save(ps,file = paste("analysis_1/ps_",outcomeConceptId,".rData",sep=""))
@@ -90,7 +95,7 @@ where ancestor_concept_id in (40241331, 21600960)
     strata <- matchOnPs(psTrimmed, caliper = 0.25, caliperScale = "standardized",maxRatio=999)
     
     #balance <- computeCovariateBalance(strata, cohortData, outcomeConceptId = outcomeConceptId)
-    #save(balance,file = paste("analysis_1/balance_",outcomeConceptId,".rData",sep=""))
+    #save(balance,file = paste("analysis_1/balance_",outcomeConceptId,".rData",sep=""))  
     
     outcomeModel <- fitOutcomeModel(outcomeConceptId, 
                                     cohortData,
@@ -105,5 +110,24 @@ where ancestor_concept_id in (40241331, 21600960)
     )
     save(outcomeModel,file = paste("analysis_1/outcomeModel_",outcomeConceptId,".rData",sep=""))
   }
-  parSapply(cluster,outcomeCounts$outcomeConceptId,runOutcome)
+  dir.create("analysis_1")
+  dummy <- sfSapply(outcomeCounts$outcomeConceptId,runOutcome)
+  
+  #Reading all the estimates into one data frame:
+  logRr <- c()
+  conceptId <- c()
+  ub <- c()
+  lb <- c()
+  for (outcomeConceptId in outcomeCounts$outcomeConceptId){
+    file = paste("analysis_1/outcomeModel_",outcomeConceptId,".rData",sep="")
+    if (file.exists(file)){
+      load(file=paste("analysis_1/outcomeModel_",outcomeConceptId,".rData",sep=""))
+      conceptId <- c(conceptId, outcomeConceptId)
+      logRr <- c(logRr,coef(outcomeModel))
+      lb <- c(lb,confint(outcomeModel)[1])
+      ub <- c(ub,confint(outcomeModel)[2])
+    }
+  }
+  data <- data.frame(conceptId = conceptId, logRr = logRr, lb = lb, ub = ub)
 }
+
