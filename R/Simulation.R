8fde11d7113988c3028b4c8b972e4e37f73f3ee9
Yuxi Tian <yuxitian@gmail.com>
2016-10-04 10:34:20 -0700

    censoring ignores covariates

--- R/Simulation.R
+++ R/Simulation.R
@@ -550,9 +550,40 @@ removeCovariates <- function(cohortMethodData,
                metaData = cohortMethodData$metaData))
 }
 
+#' Runs Simulation Study
+#'
+#' @description
+#' This function runs a simulation to compare LASSO, exposure based hdps, and bias based hdps as propensity score methods
+#'
+#' @param cohortMethodData cohortMethodData object
+#' @param confoundingScheme Type of unmeasured confounding to use for PS (0 = none; 1 = demographics; 2 = random proportion; 3 = demographics and random proportion)
+#' @param confoundingProportion Proportion of covariates to hide from propensity score as unmeasured confounding
+#' @param n Number of simulations to run (1 simulation = reroll outcomes)
+#' @param trueBeta True effect size for exposure to simulate
+#' @param outcomePrevalence Outcome prevalence to simulate; adjusts outcome baseline survival function to achieve
+#' @param crossValidate Can turn off cross validation when fitting outcome models in beginning and fitting LASSO propensity score
+#' @param hdpsFeatures TRUE = using HDPS features; FALSE = using FeatureExtraction features
+#' @param ignoreCensoring Ignore censoring altogether; sets censoring process baseline survival function to 1
+#' @param ignoreCensoringCovariates Ignore covariates effects on censoring process; only uses baseline function
+#'
+#' @return
+#' Returns the following: \describe {
+#' \item{trueOutcomeModel}{coefficients used for true outcome model}
+#' \item{trueEffectSize}{coefficient for exposure effect}
+#' \item{estimatesLasso}{logRr, bound, and sd for LASSO estimate for each simulation}
+#' \item{estimatesExpHdps}{logRr, bound, and sd for exposure based hdps}
+#' \item{estimatesBiasHdps}{logRr, bound, and sd for bias based hdps}
+#' \item{aucLasso}{auc for LASSO propensity score}
+#' \item{aucExpHdps}{auc for exposure based hdps propensity score}
+#' \item{aucBiasHdps}{auc for bias based hdps propensity score}
+#' \item{psLasso}{propensity scores for subjects in study population for LASSO}
+#' \item{psExp}{propensity scores for subjects in study population for exposure based hdps}
+#' \item{psBias}{propensity scores for subjects in study population for bias based hdps; propensity and preference scores are averaged over simulation runs}
+#' \item{originalOutcomePrevalence}{original outcome prevalence before adjustment to desired outcome prevalence}}
 #' @export
 runSimulationStudy <- function(cohortMethodData, confoundingScheme = 0, confoundingProportion = 0.3, n = 10,
-                               trueBeta = NULL, outcomePrevalence = NULL, crossValidate = TRUE, hdpsFeatures = FALSE, ignoreCensoring = FALSE) {
+                               trueBeta = NULL, outcomePrevalence = NULL, crossValidate = TRUE, hdpsFeatures = FALSE,
+                               ignoreCensoring = FALSE, ignoreCensoringCovariates = TRUE) {
   estimatesLasso = NULL
   estimatesExpHdps = NULL
   estimatesBiasHdps = NULL
@@ -576,10 +607,13 @@ runSimulationStudy <- function(cohortMethodData, confoundingScheme = 0, confound
   originalOutcomePrevalence = findOutcomePrevalence(simulationProfile$sData, simulationProfile$cData)
   if (!is.null(outcomePrevalence)) {
     if (ignoreCensoring) simulationProfile$cData$baseline = ff::as.ff(rep(1, length(simulationProfile$cData$baseline)))
+    if (ignoreCensoringCovariates) simulationProfile$cData$XB$exb = ff::as.ff(rep(1, length(simulationProfile$cData$XB$exb)))
     fun <- function(d) {return(findOutcomePrevalence(simulationProfile$sData, simulationProfile$cData, d) - outcomePrevalence)}
     delta <- uniroot(fun, lower = 0, upper = 10000)$root
     simulationProfile$sData$baseline = simulationProfile$sData$baseline^delta
   }
+
+
 
   studyPop <- createStudyPopulation(cohortMethodData = cohortMethodData,
                                     outcomeId = 3,
@@ -593,11 +627,6 @@ runSimulationStudy <- function(cohortMethodData, confoundingScheme = 0, confound
                                     riskWindowEnd = 30,
                                     addExposureDaysToEnd = TRUE)
 
-  # confoundingScheme:
-  # 0 = no confounding
-  # 1 = demographics only
-  # 2 = random only
-  # 3 = random and demographics
   covariatesToDiscard = NULL
   if (confoundingScheme == 0) {
     covariatesToDiscard = NULL
