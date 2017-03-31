6b79da210d65dfbc0b8a48062addaf7a687b7d56
Marc Suchard <msuchard@gmail.com>
2017-03-31 15:42:10 -0700

    fix case with 0 coefficients

--- R/OutcomeModels.R
+++ R/OutcomeModels.R
@@ -338,7 +338,9 @@ getOutcomeModel <- function(outcomeModel, cohortMethodData) {
   cfs <- ff::as.ram(cfs)
   cfs <- cfs[, c("coefficient", "id", "covariateName")]
   cfs$covariateName <- as.character(cfs$covariateName)
-  cfs <- cfs[order(-abs(cfs$coefficient)), ]
+  if (length(cfs$coefficient) > 0) {
+    cfs <- cfs[order(-abs(cfs$coefficient)), ]
+  }
   cfs$covariateName[cfs$id == outcomeModel$outcomeModelTreatmentVarId] <- "Treatment"
   cfs$covariateName[cfs$id == 0] <- "Intercept"
   return(cfs)
--- R/PsFunctions.R
+++ R/PsFunctions.R
@@ -180,7 +180,9 @@ getPsModel <- function(propensityScore, cohortMethodData) {
   cfs <- data.frame(coefficient = cfs, id = as.numeric(attr(cfs, "names")))
   cfs <- merge(ff::as.ffdf(cfs), cohortMethodData$covariateRef, by.x = "id", by.y = "covariateId")
   cfs <- ff::as.ram(cfs[, c("coefficient", "id", "covariateName")])
-  cfs <- cfs[order(-abs(cfs$coefficient)), ]
+  if (length(cfs$coefficient) > 0) {
+    cfs <- cfs[order(-abs(cfs$coefficient)), ]
+  }
   return(cfs)
 }
 
