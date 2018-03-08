763cea31ffe89f14ca038c50e0d25baefaf5cb17
schuemie <schuemie@ohdsi.org>
2018-03-08 00:59:50 -0500

    Vectorized summarizeAnalyses function for greater speed

--- R/RunAnalyses.R
+++ R/RunAnalyses.R
@@ -1036,48 +1036,52 @@ createReferenceTable <- function(cmAnalysisList,
 #'
 #' @export
 summarizeAnalyses <- function(referenceTable) {
-  columns <- c("analysisId", "targetId", "comparatorId", "outcomeId")
-  result <- referenceTable[, columns]
-  result$rr <- 0
-  result$ci95lb <- 0
-  result$ci95ub <- 0
-  result$p <- 1
-  result$treated <- 0
-  result$comparator <- 0
-  result$treatedDays <- NA
-  result$comparatorDays <- NA
-  result$eventsTreated <- 0
-  result$eventsComparator <- 0
-  result$logRr <- 0
-  result$seLogRr <- 0
-  for (i in 1:nrow(referenceTable)) {
-    if (referenceTable$outcomeModelFile[i] != "") {
-      outcomeModel <- readRDS(referenceTable$outcomeModelFile[i])
-      result$rr[i] <- if (is.null(coef(outcomeModel)))
+
+  summarizeOneAnalysis <- function(outcomeModelFile) {
+    result <- data.frame(rr = 0,
+                         ci95lb = 0,
+                         ci95ub = 0,
+                         p = 1,
+                         treated = 0,
+                         comparator = 0,
+                         treatedDays = NA,
+                         comparatorDays = NA,
+                         eventsTreated = 0,
+                         eventsComparator = 0,
+                         logRr = 0,
+                         seLogRr = 0)
+    if (outcomeModelFile != "") {
+      outcomeModel <- readRDS(outcomeModelFile)
+      result$rr <- if (is.null(coef(outcomeModel)))
         NA else exp(coef(outcomeModel))
-      result$ci95lb[i] <- if (is.null(coef(outcomeModel)))
+      result$ci95lb <- if (is.null(coef(outcomeModel)))
         NA else exp(confint(outcomeModel)[1])
-      result$ci95ub[i] <- if (is.null(coef(outcomeModel)))
+      result$ci95ub <- if (is.null(coef(outcomeModel)))
         NA else exp(confint(outcomeModel)[2])
       if (is.null(coef(outcomeModel))) {
-        result$p[i] <- NA
+        result$p <- NA
       } else {
         z <- coef(outcomeModel)/outcomeModel$outcomeModelTreatmentEstimate$seLogRr
-        result$p[i] <- 2 * pmin(pnorm(z), 1 - pnorm(z))
+        result$p <- 2 * pmin(pnorm(z), 1 - pnorm(z))
       }
-      result$treated[i] <- outcomeModel$populationCounts$treatedPersons
-      result$comparator[i] <- outcomeModel$populationCounts$comparatorPersons
+      result$treated <- outcomeModel$populationCounts$treatedPersons
+      result$comparator <- outcomeModel$populationCounts$comparatorPersons
       if (outcomeModel$outcomeModelType %in% c("cox", "poisson")) {
-        result$treatedDays[i] <- outcomeModel$timeAtRisk$treatedDays
-        result$comparatorDays[i] <- outcomeModel$timeAtRisk$comparatorDays
+        result$treatedDays <- outcomeModel$timeAtRisk$treatedDays
+        result$comparatorDays <- outcomeModel$timeAtRisk$comparatorDays
       }
-      result$eventsTreated[i] <- outcomeModel$outcomeCounts$treatedOutcomes
-      result$eventsComparator[i] <- outcomeModel$outcomeCounts$comparatorOutcomes
-      result$logRr[i] <- if (is.null(coef(outcomeModel)))
+      result$eventsTreated <- outcomeModel$outcomeCounts$treatedOutcomes
+      result$eventsComparator <- outcomeModel$outcomeCounts$comparatorOutcomes
+      result$logRr <- if (is.null(coef(outcomeModel)))
         NA else coef(outcomeModel)
-      result$seLogRr[i] <- if (is.null(coef(outcomeModel)))
+      result$seLogRr <- if (is.null(coef(outcomeModel)))
         NA else outcomeModel$outcomeModelTreatmentEstimate$seLogRr
     }
+    return(result)
   }
-  return(result)
+  columns <- c("analysisId", "targetId", "comparatorId", "outcomeId")
+  results <- lapply(referenceTable$outcomeModelFile, summarizeOneAnalysis)
+  results <- do.call(rbind, results)
+  results <- cbind(referenceTable[, columns], results)
+  return(results)
 }
