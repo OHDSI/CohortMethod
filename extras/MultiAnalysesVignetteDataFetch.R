0fe893bf09944c5703d86f57d4a02c563db8256e
Schuemie <MSCHUEMI@its.jnj.com>
2020-01-21 08:47:37 +0100

    Adding lit ref for IPTW. Changing vignettes to use new SqlRender interface.

--- extras/MultiAnalysesVignetteDataFetch.R
+++ extras/MultiAnalysesVignetteDataFetch.R
@@ -271,8 +271,8 @@ analysisSum <- summarizeAnalyses(result, outputFolder = "s:/temp/cohortMethodVig
 saveRDS(analysisSum, "s:/temp/cohortMethodVignette2/analysisSummary.rds")
 # cleanup:
 sql <- "DROP TABLE @resultsDatabaseSchema.outcomes"
-sql <- SqlRender::renderSql(sql, resultsDatabaseSchema = resultsDatabaseSchema)$sql
-sql <- SqlRender::translateSql(sql, targetDialect = connectionDetails$dbms)$sql
+sql <- SqlRender::render(sql, resultsDatabaseSchema = resultsDatabaseSchema)
+sql <- SqlRender::translate(sql, targetDialect = connectionDetails$dbms)
 connection <- DatabaseConnector::connect(connectionDetails)
 DatabaseConnector::executeSql(connection, sql)
 DatabaseConnector::disconnect(connection)
