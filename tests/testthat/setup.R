d1015e3ea88844180ae404ce461ec57b94a2949d
jgilber2 <JGIlber2@its.jnj.com>
2021-07-15 10:41:50 -0700

    fix to setup.R

--- tests/testthat/setup.R
+++ tests/testthat/setup.R
@@ -2,7 +2,7 @@ library(Eunomia)
 connectionDetails <- getEunomiaConnectionDetails()
 Eunomia::createCohorts(connectionDetails)
 
-withr::defer(testthat::teardown_env(), {
+withr::defer({
   # Remove the Eunomia database:
   unlink(connectionDetails$server())
-})
+}, testthat::teardown_env())
