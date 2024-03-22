9d43b7c420897ab434072d4a9148e75e4384089f
Jamie Gilbert <JGilber2@its.jnj.com>
2024-03-22 13:13:22 -0700

    Missing columns

--- inst/sql/postgresql/migrations/Migration_2-v5.2.2.sql
+++ inst/sql/postgresql/migrations/Migration_2-v5.2.2.sql
@@ -21,10 +21,20 @@ ALTER TABLE @database_schema.@table_prefixcm_interaction_result ALTER COLUMN tar
 ALTER TABLE @database_schema.@table_prefixcm_interaction_result ALTER COLUMN comparator_id TYPE BIGINT;
 ALTER TABLE @database_schema.@table_prefixcm_interaction_result ALTER COLUMN outcome_id TYPE BIGINT;
 
+
+ALTER TABLE @database_schema.@table_prefixcm_covariate_balance ALTER COLUMN target_id TYPE BIGINT;
+ALTER TABLE @database_schema.@table_prefixcm_covariate_balance ALTER COLUMN comparator_id TYPE BIGINT;
+ALTER TABLE @database_schema.@table_prefixcm_covariate_balance ALTER COLUMN outcome_id TYPE BIGINT;
+
+ALTER TABLE @database_schema.@table_prefixcm_diagnostics_summary ALTER COLUMN target_id TYPE BIGINT;
+ALTER TABLE @database_schema.@table_prefixcm_diagnostics_summary ALTER COLUMN comparator_id TYPE BIGINT;
+ALTER TABLE @database_schema.@table_prefixcm_diagnostics_summary ALTER COLUMN outcome_id TYPE BIGINT;
+
 -- Alter columns in cm_target_comparator_outcome table
 ALTER TABLE @database_schema.@table_prefixcm_target_comparator_outcome ALTER COLUMN outcome_id TYPE BIGINT;
 ALTER TABLE @database_schema.@table_prefixcm_target_comparator_outcome ALTER COLUMN target_id TYPE BIGINT;
 ALTER TABLE @database_schema.@table_prefixcm_target_comparator_outcome ALTER COLUMN comparator_id TYPE BIGINT;
+ALTER TABLE @database_schema.@table_prefixcm_target_comparator_outcome ALTER COLUMN outcome_of_interest BIGINT;
 
 -- Alter columns in cm_kaplan_meier_dist table
 ALTER TABLE @database_schema.@table_prefixcm_kaplan_meier_dist ALTER COLUMN target_id TYPE BIGINT;
@@ -35,6 +45,13 @@ ALTER TABLE @database_schema.@table_prefixcm_kaplan_meier_dist ALTER COLUMN outc
 ALTER TABLE @database_schema.@table_prefixcm_likelihood_profile ALTER COLUMN target_id TYPE BIGINT;
 ALTER TABLE @database_schema.@table_prefixcm_likelihood_profile ALTER COLUMN comparator_id TYPE BIGINT;
 ALTER TABLE @database_schema.@table_prefixcm_likelihood_profile ALTER COLUMN outcome_id TYPE BIGINT;
+
+ALTER TABLE @database_schema.@table_prefixcm_propensity_model ALTER COLUMN target_id TYPE BIGINT;
+ALTER TABLE @database_schema.@table_preficm_propensity_model ALTER COLUMN comparator_id TYPE BIGINT
+
+ALTER TABLE @database_schema.@table_prefixcm_preference_score_dist ALTER COLUMN target_id TYPE BIGINT;
+ALTER TABLE @database_schema.@table_prefixcm_preference_score_dist ALTER COLUMN comparator_id TYPE BIGINT
+
 
 -- Alter columns in cm_shared_covariate_balance table
 ALTER TABLE @database_schema.@table_prefixcm_shared_covariate_balance ALTER COLUMN target_id TYPE BIGINT;
